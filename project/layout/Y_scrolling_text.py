97da7fb586d13960201b0fc315ef46ae43c6b67d
enjules <enjules31@gmail.com>
2025-05-22 18:58:13 +0200

    FIX : suppression de l'ancien projet

--- project/layout/Y_scrolling_text.py
+++
@@ -1,6 +0,0 @@
-import dearpygui.dearpygui as dpg
-import time
-
-def Y_scrolling_test(text="", parent=None):
-    x = dpg.add_text(label=text)
-    print(dpg.get_item_user_data(parent))
--- project/objects/songs/Song_from_gdrive.py
+++
@@ -1,8 +0,0 @@
-from project.objects.songs import Song
-
-class Song_from_gdrive(Song):
-
-    def __init__(self, folder, name, path, parent_id, id=None, id_gdrive=None):
-        super().__init__(folder, name, path, parent_id, id=id)
-
-        self.id_gdrive = id_gdrive
